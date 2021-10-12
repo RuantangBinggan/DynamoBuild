a5350de96c022a29e75f02b95e86abf90307f3a9
RuantangBinggan <784958041@QQ.COM>
2021-10-13 00:20:21 +0800

    添加项目文件。

---
+++ DynamoBuild/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Revit.GeometryConversion;
+using DG = Autodesk.DesignScript.Geometry;
+
+
+namespace DynamoTest
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        Result IExternalCommand.Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document document = commandData.Application.ActiveUIDocument.Document;
+            try
+            {
+                DG.Point startPoint = DG.Point.ByCoordinates(0, 0, 0);
+                DG.Point endPoint = DG.Point.ByCoordinates(1000, 0, 0);
+                DG.Line line = DG.Line.ByStartPointEndPoint(startPoint, endPoint);
+                Line revitLine = line.ToRevitType() as Line; //将Dynamo的Geometry转化为Revit的Geometry
+                using (Transaction transaction = new Transaction(document, "Create Line"))
+                {
+                    transaction.Start();
+                    ModelCurve modelCurve = document.Create.NewModelCurve(revitLine,
+                        SketchPlane.Create(document, Plane.CreateByNormalAndOrigin(XYZ.BasisZ, XYZ.Zero)));
+                    transaction.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return Result.Succeeded;
+
+
+        }
+    }
+}
+
---
+++ DynamoBuild/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Dynamo.Applications;
+
+namespace DynamoBuild
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        Result IExternalCommand.Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string Journal_Dynamo_Path = @"C:\Users\zyx\Desktop\2RevitArcBridge\Dynamo2Revit\Dynamo2Revit\22.dyn";  //一个空的dynamo文件
+
+            DynamoRevit dynamoRevit = new DynamoRevit();
+
+            DynamoRevitCommandData dynamoRevitCommandData = new DynamoRevitCommandData();
+            dynamoRevitCommandData.Application = commandData.Application;
+            IDictionary<string, string> journalData = new Dictionary<string, string>
+            {
+                { Dynamo.Applications.JournalKeys.ShowUiKey, false.ToString() }, // don't show DynamoUI at runtime
+                { Dynamo.Applications.JournalKeys.AutomationModeKey, true.ToString() }, //run journal automatically
+                { Dynamo.Applications.JournalKeys.DynPathKey, Journal_Dynamo_Path }, //run node at this file path
+                { Dynamo.Applications.JournalKeys.DynPathExecuteKey, true.ToString() }, // The journal file can specify if the Dynamo workspace opened from DynPathKey will be executed or not. If we are in automation mode the workspace will be executed regardless of this key.
+                { Dynamo.Applications.JournalKeys.ForceManualRunKey, false.ToString() }, // don't run in manual mode
+                { Dynamo.Applications.JournalKeys.ModelShutDownKey, true.ToString() }
+
+            };
+
+            dynamoRevitCommandData.JournalData = journalData;
+            Result externalCommandResult = dynamoRevit.ExecuteCommand(dynamoRevitCommandData);
+            return externalCommandResult;
+
+        }
+    }
+}
+
